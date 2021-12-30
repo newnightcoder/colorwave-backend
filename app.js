bb01b667453512e5a921917b58b72c274b767831
Nightcoder <newnightcoder@gmail.com>
2021-12-30 12:09:25 +0100

    endpoint added for stripe pk retrieval from frontend

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import express from "express";
 import stripe from "stripe";
 
 const app = express();
-const PORT = 3002;
+const PORT = 4242;
 
 app.use(cors());
 app.use(express.json());
@@ -12,17 +12,22 @@ app.use(express.urlencoded({ extended: true }));
 
 const stripeConnexion = stripe(`${process.env.STRIPE_SECRET_KEY}`);
 
+app.get("/", (req, res, next) => {
+  res.send({ key: process.env.STRIPE_PUBLIC_KEY });
+});
+
 app.post("/payment-intent-secret", async (req, res) => {
+  //PAYMENT INTENT CREATION
   const paymentIntent = await stripeConnexion.paymentIntents.create({
     amount: 2000,
     currency: "eur",
-    payment_method_types: ["card", "sepa_debit"],
-    // automatic_payment_methods: {
-    //   enabled: true,
-    // },
+    // payment_method_types: ["card"],
+    automatic_payment_methods: {
+      enabled: true,
+    },
   });
   console.log("request received!");
-  res.json({ clientSecret: paymentIntent.client_secret });
+  res.send({ clientSecret: paymentIntent.client_secret });
 });
 
 app.listen(PORT, (err) => {
