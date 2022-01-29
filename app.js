3f748ecbe07065a0f15e8a2a86d4254e24d23f34
Nightcoder <newnightcoder@gmail.com>
2022-01-29 02:57:53 +0100

    herokupostbuild+app.js update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const stripeConnexion = stripe(`${process.env.STRIPE_SECRET_KEY}`);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "../client/build")));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 }
