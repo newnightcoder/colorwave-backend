a6f39cc76ed2628ed786193276b71285315eb2f4
Nightcoder <newnightcoder@gmail.com>
2022-01-29 13:56:57 +0100

    stripe confirmation url correction

--- client/src/Components/CheckoutForm.jsx
+++ client/src/Components/CheckoutForm.jsx
@@ -21,7 +21,7 @@ const CheckoutForm = ({ formValidated }) => {
       const { error } = await stripe.confirmPayment({
         elements,
         confirmParams: {
-          return_url: "http://localhost:3001/success",
+          return_url: "https://colorwave-shop.herokuapp.com/success",
         },
       });
       if (error) return setIsLoading(false);
