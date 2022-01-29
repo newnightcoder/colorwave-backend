249f789c645adf3c176d968ba605b695be016220
Nightcoder <newnightcoder@gmail.com>
2022-01-29 03:01:52 +0100

    correction ../client/ to ./client

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.use(express.urlencoded({ extended: true }));
 const stripeConnexion = stripe(`${process.env.STRIPE_SECRET_KEY}`);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../client/build")));
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
 }
 
