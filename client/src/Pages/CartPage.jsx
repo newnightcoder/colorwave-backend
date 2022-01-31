1c3d0f42c5558a4bc1ea82cba6d5ed35b004b8a6
Nightcoder <newnightcoder@gmail.com>
2022-01-31 23:43:14 +0100

    form mobile corrections

--- client/src/Pages/CartPage.jsx
+++ client/src/Pages/CartPage.jsx
@@ -254,8 +254,8 @@ const CartPage = () => {
       <Elements stripe={stripePromise} options={options}>
         <Steps formOpen={formOpen} formValidated={formValidated} />
         <Div100vh
-          className="transition-transform duration-1000 bg-black"
-          style={{ transform: formOpen && `translateY(calc(-100% + 64px))`, overflowY: formOpen ? "scroll" : "hidden" }}
+          className="transition-transform duration-1000"
+          style={{ transform: formOpen && `translateY(calc(-100% + 64px))`, overflowY: formOpen ? "auto" : "hidden" }}
         >
           <Div100vh className="page transition-transform duration-1000 border-4 border-red-500 pt-16 md:pt-24 relative w-screen font-cabin flex flex-col items-center justify-center bg-sound overflow-y-hidden">
             <div
