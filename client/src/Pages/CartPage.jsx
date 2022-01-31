b757ff2fd57296035c5c5c8af1bc7435a0b9916c
Nightcoder <newnightcoder@gmail.com>
2022-01-31 01:12:35 +0100

    100vh coorection

--- client/src/Pages/CartPage.jsx
+++ client/src/Pages/CartPage.jsx
@@ -256,7 +256,8 @@ const CartPage = () => {
         {/* WHOLE PAGE */}
 
         <div
-          style={{ height: width < 768 ? `calc(100vh - 4rem)` : `calc(100vh - 5.5rem)` }}
+          style={{ height: `calc(${responsiveHeight} - 4rem)` }}
+          // style={{ height: width < 768 ? `calc(100vh - 4rem)` : `calc(100vh - 5.5rem)` }}
           className="page relative w-screen overflow-y-hidden font-cabin flex flex-col items-center justify-center bg-sound md:mt-8"
         >
           <Steps formOpen={formOpen} formValidated={formValidated} />
@@ -266,7 +267,7 @@ const CartPage = () => {
             style={{ contain: "content" }}
             className="page-container h-full w-full relative overflow-y-hidden flex flex-col items-start justify-start bg-sound"
           >
-            <div style={{ height: `calc(${responsiveHeight} - 4rem)` }} className="w-full flex flex-col md:flex-row">
+            <div className="h-full w-full flex flex-col md:flex-row">
               <div className="h-full w-full md:w-3/5">
                 <div
                   className="cart-content-wrapper h-max w-full transition-transform duration-300"
