e3b230b07df8208aab45c4796097b4f5977ac418
Nightcoder <newnightcoder@gmail.com>
2022-01-31 03:23:23 +0100

    height correction

--- client/src/Pages/CartPage.jsx
+++ client/src/Pages/CartPage.jsx
@@ -306,7 +306,7 @@ const CartPage = () => {
                 </div>
                 <CheckoutForm formValidated={formValidated} />
               </div>
-              <div className="h-max md:h-full w-full relative md:w-2/5 flex items-center justify-center ">
+              <div className="h-max md:h-full w-full border-4 border-red-500 relative md:w-2/5 flex items-center justify-center ">
                 {items.length !== 0 && (
                   <CartRecap
                     totalPrice={totalPrice}
